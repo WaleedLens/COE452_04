1bc6c674d99ad559d4d221a48d874de97d6d45b4
Waleed <90866317+WaleedLens@users.noreply.github.com>
2024-03-27 20:40:15 +0000

    Initial commit

---
+++ sub.js
@@ -0,0 +1,30 @@
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('sub', (req, res) => {
+  // Default values for X and Y
+  let X = 0;
+  let Y = 0;
+
+  if (req.query.data) {
+    try {
+      const data = JSON.parse(req.query.data);
+      X = data.X;
+      Y = data.Y;
+    } catch (error) {
+      res.status(400).send({error: "Invalid JSON format in query parameter."});
+      return;
+    }
+  }
+
+  const result = X - Y; 
+
+
+  const responseObject = {
+    X: X,
+    Y: Y,
+    result: result
+  };
+
+
+  res.json(responseObject);
+});
